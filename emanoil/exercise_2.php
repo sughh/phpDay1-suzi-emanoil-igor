7e7c476ee8ac1fa3dc0e81ac06f24f4e5365ccc5
Emanoil Karov <e_karov@yahoo.com>
2022-11-07 11:08:38 +0100

    Excises 1, 2 and 3 added

---
+++ emanoil/exercise_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 2</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+        $name = "Arthur Dent";
+        $age = 42;
+        $jobTitle = "journalist";
+
+        echo "<h3>hi, my name is $name, and i am $age, and i work as a $jobTitle.</h3>"
+    ?>
+    
+</body>
+</html>
---
+++ emanoil/exercise_3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 3</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+        $arr = [ "Mark", "John", "Georg", "Lisa"];
+        echo "<h3>the third player in the team is $arr[2]</h3>";
+    ?>
+</body>
+</html>
